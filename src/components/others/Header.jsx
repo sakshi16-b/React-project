0352055580f8eec53a353b71b8f756684c0f26fe
sakshi16-b <sakshibisht016@gmail.com>
2025-07-14 21:27:45 +0530

    login functionality done

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
 
         <div className='flex items-end justify-between'>
-            <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'> User👋</span></h1>
+            <h1 className='text-2xl text-white font-medium'>Hello <br /><span className='text-3xl text-white font-semibold'> User👋</span></h1>
             <button className='bg-red-500 text-white px-5 py-2 text-lg font-medium rounded-sm'>Log Out</button>
         </div>
 
